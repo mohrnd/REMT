45c915a857d9dc56ab43b3943b56bf304ddf6b7a
mohrnd <mohren251@gmail.com>
2024-03-07 12:28:45 +0100

    added task scheduling

--- Tests/ssh-sessions/test-fabric-(not interactive).py
+++ Tests/ssh-sessions/test-fabric-(not interactive).py
@@ -1,62 +1,17 @@
-import getpass
-from fabric import Connection, Config
-from termcolor import colored
+from fabric import Connection
 
-# fixed sudo and cd, everything should be working as intended, just need to do some more tests
-#issue in the script below: command outputs are being printed 2 times
+hostname = '192.168.69.40'
+user = 'root'
+port = 22 
+password = 'Pa$$w0rd' 
+cron_job = '*/5 * * * 2 perl /home/mobman/wad2.pl'
 
 
-hostname = "192.168.69.40"
-port = 22
-user = "manager1"
-password = "Pa$$w0rd"
+conn = Connection(hostname, user=user, port=port, connect_kwargs={"password": password})
 
-config = Config(overrides={'sudo': {'password': password}})
+result = conn.run(f'echo "{cron_job}" >> /var/spool/cron/{user}', warn=True)
 
-def get_prompt(conn):
-    result = conn.run('pwd', hide=True)
-    cwd = result.stdout.strip()
-    return colored(f'{user}@{hostname}:{cwd}$ ', 'green')
-
-def exec_cmd(conn):
-    while True:
-        prompt = get_prompt(conn)
-        command = input(prompt)
-        if command.lower() == 'exit':
-            break
-        if "cd " in command:
-            exec_cd(conn, command)
-        elif "sudo " in command:
-            exec_sudo(conn, command)
-        else:
-            result = conn.run(command, warn=True)
-            if result.exited:
-                print(colored(f"Command exited with status {result.exited}", 'red'))
-            if result.stdout:
-                print(result.stdout.strip())
-            if result.stderr:
-                print(colored(result.stderr.strip(), 'red'))
-
-def exec_cd(conn, cmd):
-    directory = cmd.split("cd ")[1].strip()
-    with conn.cd(directory):
-        pass
-
-def exec_sudo(conn, cmd):
-    cmd_no_sudo = cmd.split("sudo ")[1].strip()
-    result = conn.sudo(cmd_no_sudo, warn=True)
-    if result.exited:
-        print(colored(f"Command exited with status {result.exited}", 'red'))
-    if result.stdout:
-        print(result.stdout.strip())
-    if result.stderr:
-        print(colored(result.stderr.strip(), 'red'))
-
-try:
-    with Connection(host=hostname, user=user, port=port, connect_kwargs={"password": password}, config=config) as conn:
-        conn.sudo('dnf install gedit', warn=True)# exec_cmd(conn)
-
-except KeyboardInterrupt:
-    print("\nExited by user.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+if result.exited != 0:
+    print("Failed to add cron job:", result.stderr)
+else:
+    print("Cron job added successfully.")
---
+++ Tests/task_scheduling/task_scheduler_windows.py
@@ -0,0 +1,51 @@
+import paramiko
+# Adding jobs works !!
+# Removing jobs doesnt work
+
+def test_cron(ssh_client, cron):
+    # Check cron syntax validity
+    stdin, stdout, stderr = ssh_client.exec_command(f'echo "{cron}" | crontab -l', get_pty=True)
+    error = stderr.read().decode().strip()
+    if error:
+        return error
+    return True
+
+def add_cron(ssh_client, job):
+    # Add cron job
+    if test_cron(ssh_client, job) is True:
+        stdin, stdout, stderr = ssh_client.exec_command(f'(crontab -l; echo "{job}") | crontab -', get_pty=True)
+        print("Cron job added successfully!")
+        print("Output:", stdout.read().decode().strip())
+        print("Error:", stderr.read().decode().strip())
+    else:
+        print("Syntax Error:", test_cron(ssh_client, job))
+
+def remove_cron(ssh_client, job):
+    # Remove cron job
+    stdin, stdout, stderr = ssh_client.exec_command(f'crontab -l | grep -v "{job}" | crontab -', get_pty=True)
+    print("Cron job removed successfully!")
+    print("Output:", stdout.read().decode().strip())
+    print("Error:", stderr.read().decode().strip())
+
+if __name__ == "__main__":
+    # SSH credentials and job
+    host = '192.168.69.40'
+    port = 22  # Default SSH port
+    username = 'manager1'
+    password = 'Pa$$w0rd'
+    jobs = ["*/5 * * * * /path/to/command2", "*/5 * * * * /path/to/test", "*/5 * * * * /path/to/command1"]
+
+    # Connect to remote machine
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=host, port=port, username=username, password=password)
+
+    # Add job
+    for job in jobs:
+        add_cron(ssh_client, job)
+
+    # Remove job
+    # remove_cron(ssh_client, job)
+
+    # Close SSH connection
+    ssh_client.close()
