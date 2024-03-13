0ef8daed0dd9c958845e0bb12d5805fbcbd01dc0
LamariAzzeddine <azzedinelamari66rgmail.com>
2024-03-13 18:25:36 +0100

    EDITEUR_DE_CODE_AVEC_LENVOIE_DE_FICHIERS

--- Tests/editeur_de_code/editeur.py
+++ Tests/editeur_de_code/editeur.py
@@ -4,6 +4,8 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import subprocess
 import csv
+import os
+from transfert import Transfer
 
 class PythonHighlighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
@@ -61,11 +63,12 @@ class CodeEditor(QPlainTextEdit):
         self.highlighter = PythonHighlighter(self.document())
 
 class OnlineIPDialog(QDialog):
-    def __init__(self, online_ips):
+    def __init__(self, online_ips, current_file_path):
         super().__init__()
 
         self.setWindowTitle("Adresses IP en ligne")
         self.online_ips = online_ips
+        self.current_file_path = current_file_path
         self.initUI()
 
     def initUI(self):
@@ -78,6 +81,10 @@ class OnlineIPDialog(QDialog):
             layout.addWidget(ip_checkbox)
             self.checkbox_list.append(ip_checkbox)
 
+        send_button = QPushButton("Envoyer")
+        send_button.clicked.connect(self.send_files)
+        layout.addWidget(send_button)
+
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
@@ -85,12 +92,34 @@ class OnlineIPDialog(QDialog):
 
         self.setLayout(layout)
 
-    def get_checked_ips(self):
-        checked_ips = []
-        for checkbox in self.checkbox_list:
-            if checkbox.isChecked():
-                checked_ips.append(checkbox.text())
-        return checked_ips
+    def send_files(self):
+        checked_ips = [checkbox.text() for checkbox in self.checkbox_list if checkbox.isChecked()]
+        if not checked_ips:
+            QMessageBox.warning(self, "Aucune adresse IP sélectionnée", "Veuillez sélectionner au moins une adresse IP.")
+            return
+
+        if not self.current_file_path:
+            QMessageBox.warning(self, "Fichier non sauvegardé", "Veuillez d'abord sauvegarder le fichier avant de l'envoyer.")
+            return
+
+        local_path = self.current_file_path
+        remote_path = "/home/user2/Bureau/" + os.path.basename(local_path)
+                            #change it by: "/etc/remt" + os.path.basename(local_path)
+
+        success_ips = []
+        for ip in checked_ips:
+            for ip_data in self.online_ips:
+                if ip_data['ip'] == ip:
+                    username = ip_data['linux_username']
+                    password = ip_data['password']
+                    result = Transfer.PUT(ip, username, password, local_path, remote_path)
+                    print(result)
+                    success_ips.append(ip)
+                    break
+
+        if success_ips:
+            success_message = "Le fichier a été envoyé avec succès aux adresses IP suivantes :\n" + "\n".join(success_ips)
+            QMessageBox.information(self, "Fichier envoyé", success_message)
 
 class mon_editeur(QMainWindow):
     def __init__(self):
@@ -102,9 +131,10 @@ class mon_editeur(QMainWindow):
         self.setCentralWidget(self.editor)
         self.setWindowTitle('Éditeur de code')
         self.resize(630,400)  
+        self.setFixedSize(self.size())  # Empêcher le redimensionnement de la fenêtre
         self.create_tool_bar()
         
-        style_file = QFile("../REMT/Tests/editeur_de_code/style.qss")
+        style_file = QFile(r"C:\Users\dell-5320\Desktop\ide\editeur_de_code\style.qss")
         style_file.open(QFile.ReadOnly | QFile.Text)
         stream = QTextStream(style_file)
         app.setStyleSheet(stream.readAll())
@@ -112,22 +142,28 @@ class mon_editeur(QMainWindow):
     def create_tool_bar(self):
         toolbar = QToolBar()
         
-        openIcon = QIcon("../REMT/Tests/editeur_de_code/open.png")  
+        newIcon = QIcon(r"C:\Users\dell-5320\Desktop\ide\editeur_de_code\new.png")  
+        newBtn = QPushButton(newIcon, 'Nouveau', self)
+        newBtn.clicked.connect(self.newFile)
+        toolbar.addWidget(newBtn)
+        
+        openIcon = QIcon(r"C:\Users\dell-5320\Desktop\ide\editeur_de_code\open.png")  
         openBtn = QPushButton(openIcon, 'Ouvrir', self)
         openBtn.clicked.connect(self.openFile)
         toolbar.addWidget(openBtn)
         
-        saveIcon = QIcon("../REMT/Tests/editeur_de_code/save.png")  
+        saveIcon = QIcon(r"C:\Users\dell-5320\Desktop\ide\editeur_de_code\save.png")  
         saveBtn = QPushButton(saveIcon, 'Sauvegarder', self)
         saveBtn.clicked.connect(self.saveFile)
         toolbar.addWidget(saveBtn)
         
-        executeIcon = QIcon("../REMT/Tests/editeur_de_code/execute.png")  
+        
+        executeIcon = QIcon(r"C:\Users\dell-5320\Desktop\ide\editeur_de_code\execute.png")  
         executeBtn = QPushButton(executeIcon, 'Exécuter Script Shell', self)
         executeBtn.clicked.connect(self.executeScript)
         toolbar.addWidget(executeBtn)
 
-        showOnlineIPsBtn = QPushButton("Afficher les IP en ligne")
+        showOnlineIPsBtn = QPushButton("Envoyer Le Fichier")
         showOnlineIPsBtn.clicked.connect(self.show_online_ips)
         toolbar.addWidget(showOnlineIPsBtn)
 
@@ -147,38 +183,32 @@ class mon_editeur(QMainWindow):
             QMessageBox.warning(self, "Aucun script", "Veuillez écrire un script dans l'éditeur.")
 
     def saveFile(self):
-        print(self.path)
         if self.path == '':
-            self.file_saves()
+            self.path, _ = QFileDialog.getSaveFileName(self, "Enregistrer le fichier", "", "Documents texte (*.txt)")
+            if self.path == '':
+                return   
         text = self.editor.toPlainText()
         try:
             with open(self.path, 'w') as f:
                 f.write(text)
-                self.update_title()
+                self.setWindowTitle(f'Éditeur de code - {os.path.basename(self.path)}')  # Mettre à jour le titre de la fenêtre avec le nom du fichier
         except Exception as e:
-            print(e)    
-            
-    def file_saves(self):
-        self.path, _ = QFileDialog.getSaveFileName(self, "Enregistrer le fichier", "", "Documents texte (*.txt);Tous les fichiers (*)")
-        if self.path == '':
-            return   
-        text = self.editor.toPlainText()
-        try:
-            with open(self.path, 'w') as f:
-                f.write(text)
-                self.update_title()
-        except Exception as e:
-            print(e)    
+            print(e)
+
+    def newFile(self):
+        self.path = ""
+        self.editor.clear()
+        self.setWindowTitle('Éditeur de code')
 
     def openFile(self):
-        self.path, _ = QFileDialog.getOpenFileName(self, "Ouvrir un fichier", "", "Documents texte (*.txt);Tous les fichiers (*)")
+        self.path, _ = QFileDialog.getOpenFileName(self, "Ouvrir un fichier", "", "Documents texte (*.txt)")
         if self.path == '':
             return
         try:
             with open(self.path, 'r') as f:
                 text = f.read()
                 self.editor.setPlainText(text)
-                self.update_title()
+                self.setWindowTitle(f'Éditeur de code - {os.path.basename(self.path)}')  # Mettre à jour le titre de la fenêtre avec le nom du fichier
         except Exception as e:
             print(e)
 
@@ -195,23 +225,12 @@ class mon_editeur(QMainWindow):
         return online_ips
 
     def show_online_ips(self):
-      
-        csv_path = "../REMT/Tests/editeur_de_code/snmp_users.csv"
-        
+        csv_path = r"C:\Users\dell-5320\Desktop\ide\editeur_de_code\snmp_users.csv"
         online_ips = self.extract_online_ips(csv_path)
-        
-        dialog = OnlineIPDialog(online_ips)
-        if dialog.exec_():
-            checked_ips = dialog.get_checked_ips()
-            print("Adresses IP sélectionnées :", checked_ips)
+        dialog = OnlineIPDialog(online_ips, self.path)
+        dialog.exec_()
 
 app = QApplication(sys.argv)
 window = mon_editeur()
 window.show()
 sys.exit(app.exec_())
-
-# TODO:
-     # CHANGE THE ICONS (YOU CAN FIND BETTER ONES IN \REMT\Tests\manager GUI tests)!!!
-     # ADD ERROR HANDLING AND WARNINGS
-     # CLEAN THE SCRIPT (REWRITE THE WHOLE THING IN ENGLISH)
-    
