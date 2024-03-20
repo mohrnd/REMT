053b846d7e72d65eb888c10714d1ca703f547133
mohrnd <mohren251@gmail.com>
2024-03-20 13:30:53 +0100

    added functions, changed a bunch of things

--- Tests/task_scheduling/Task_scheduler_main.py
+++ Tests/task_scheduling/Task_scheduler_main.py
@@ -6,12 +6,46 @@ from PyQt5.QtGui import *
 from qfluentwidgets import (NavigationItemPosition, MessageBox, setTheme, setThemeColor, Theme, FluentWindow,
                             NavigationAvatarWidget, SubtitleLabel, setFont, InfoBadge,
                             InfoBadgePosition, CheckBox)
+from TaskScheduler import *
+import os
+
 class MainWindow(QWidget, Ui_Frame):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.add_button.clicked.connect(self.test)
+        self.Apply.clicked.connect(self.Apply)
         
+    def show_active_crons(self):
+        pass
+    
+    def show_active_machines(self):
+        pass
+    
+    def Check_ip(hostname):
+        param = '-n' if os.name.lower() == 'nt' else '-c'
+        response = os.system(f"ping {param} 1 -w 100 {hostname} > NUL 2>&1")   # 100 ms wait time, might change it later
+        if response == 0:
+            return True
+        else:
+            return False
+    
+    
+    
+    
+    def Apply(self):
+        selectedIPS = []
+        for row in range(self.Machines.rowCount()):
+            checkbox_item = self.Machines.cellWidget(row, 2)
+            if isinstance(checkbox_item, CheckBox) and checkbox_item.isChecked():
+                ip_address_item = self.Machines.item(row, 0)
+                if ip_address_item is not None:
+                    selectedIPS.append(ip_address_item.text())
+        print(selectedIPS)
+        
+        ### add the add_cron method
+    
+
     def test(self):
         text = self.minute_input.text()
         if text == '':
@@ -36,6 +70,7 @@ def main():
     color = QColor('#351392')
     setThemeColor(color ,Qt.GlobalColor , '') 
     
+    
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
@@ -43,3 +78,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
