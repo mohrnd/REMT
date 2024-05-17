3e7bf3aa2f399acfca5880157904c63ce5212bde
mohrnd <mohren251@gmail.com>
2024-05-17 16:54:06 +0100

    Finished the trap viewer interface

---
+++ Tests/Traps viewer/Main_Trap_Viewer.py
@@ -0,0 +1,82 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QCompleter
+from qfluentwidgets import setThemeColor
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QColor
+import re
+from Ui_Traps_Viewer_interface import Ui_Form
+
+class MainWindow(Ui_Form, QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.log_file_path = r"..\REMT\TrapsReceived.log"  # Update with the actual log file path
+        self.stands = ["SystemStartup", "SystemShutdown"]
+        self.ip_addresses = set()
+        self.load_log_file()
+
+        self.Filter_Button.clicked.connect(self.filter_traps)
+
+        completer_data = list(self.ip_addresses) + self.stands
+        self.completer = QCompleter(completer_data, self.Filter)
+        self.completer.setCaseSensitivity(Qt.CaseInsensitive)
+        self.completer.setMaxVisibleItems(10)
+        self.Filter.setCompleter(self.completer)
+
+        # Create a QTimer for updating every 60 seconds
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_content)
+        self.timer.start(60000)  # 60000 milliseconds = 60 seconds
+
+    def load_log_file(self):
+        try:
+            with open(self.log_file_path, 'r') as file:
+                content = file.read()
+                self.All_Traps_received.setPlainText(content)
+                self.extract_ip_addresses(content)
+                # self.extract_latest_trap(content)
+        except Exception as e:
+            self.All_Traps_received.setPlainText(f"Error reading log file: {e}")
+
+    def extract_ip_addresses(self, content):
+        lines = content.split('\n')
+        ip_pattern = re.compile(r'Trap from "(\d+\.\d+\.\d+\.\d+)"')
+        for line in lines:
+            match = ip_pattern.search(line)
+            if match:
+                self.ip_addresses.add(match.group(1))
+
+    def filter_traps(self):
+        filter_text = self.Filter.text()
+        if not filter_text:
+            return
+
+        try:
+            with open(self.log_file_path, 'r') as file:
+                lines = file.readlines()
+
+            filtered_lines = []
+            for line in lines:
+                if filter_text in line:
+                    filtered_lines.append(line)
+
+            self.All_Traps_received.setPlainText(''.join(filtered_lines))
+        except Exception as e:
+            self.All_Traps_received.setPlainText(f"Error filtering log file: {e}")
+
+
+    def update_content(self):
+        # Call load_log_file to update the content
+        self.load_log_file()
+
+
+def main():
+    app = QApplication(sys.argv)
+    color = QColor('#351392')
+    setThemeColor(color.name(), Qt.GlobalColor, '')
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ Tests/network monitoring/snmp tests/SNMPV3_trap_receiver_v3.py
@@ -0,0 +1,116 @@
+from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.entity import engine, config
+from pysnmp.entity.rfc3413 import ntfrcv
+from pysnmp.proto.api import v2c
+import datetime
+import logging
+import csv
+
+# Import win_notif function from notifications module
+from notifications import win_notif
+
+logging.basicConfig(filename='TrapsReceived.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def add_snmp_users_from_csv(CSV_File_Path, snmpEngine):
+    with open(CSV_File_Path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            username = row['SNMPv3_username']
+            auth_protocol = row['auth_Protocole']
+            priv_protocol = row['Priv_Protocole']
+            if auth_protocol == 'SHA':
+                auth_protocol = getattr(config, 'usmHMACSHAAuthProtocol')
+            elif auth_protocol == 'MD5':
+                auth_protocol = getattr(config, 'usmHMACMD5AuthProtocol')
+            if priv_protocol == 'DES':
+                priv_protocol = getattr(config, 'usmDESPrivProtocol')
+            elif priv_protocol == 'AES':
+                priv_protocol = getattr(config, 'usmAesCfb128Protocol')
+                
+            auth_password = row['auth_password']
+            priv_password = row['priv_password']
+            security_engine_id = v2c.OctetString(hexValue=row['security_engine_id'])
+            # SNMPv3 setup
+            config.addV3User(
+                snmpEngine,
+                username,
+                auth_protocol,
+                auth_password,
+                priv_protocol,
+                priv_password,
+                securityEngineId=security_engine_id
+            )
+
+def interpret_oid(oid):
+    oid_mappings = {
+        '1.3.6.1.2.1.1.3.0': 'SysUptime',
+        '1.3.6.1.6.3.1.1.4.1.0': 'SNMPTrapOID',
+        '1.3.6.1.6.3.1.1.4.3.0': 'SNMPTrapEnterprise'
+    }
+    return oid_mappings.get(oid, oid)
+
+def cbFun(snmpEngine, stateReference, contextEngineId, contextName, varBinds, cbCtx):
+    transport_info = snmpEngine.msgAndPduDsp.getTransportInfo(stateReference)
+    log_message_parts = []  # List to collect parts of the log message
+
+    if transport_info:
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        ip_address = transport_info[1][0]
+        log_message_parts.append(f'Trap from "{ip_address}"')
+        notification_title = 'SNMP Trap Received'
+        notification_message = f'Trap from {ip_address} received at {current_time}'
+        notification_icon = None
+        win_notif(notification_title, notification_message, notification_icon)
+    else:
+        log_message_parts.append('error')
+
+    for name, val in varBinds:
+        oid_str = name.prettyPrint()
+        value_str = val.prettyPrint()
+        interpreted_oid = interpret_oid(oid_str)
+        log_message_parts.append(f'{interpreted_oid} = {value_str}')
+        
+        with open('../REMT/Tests/network monitoring/snmp tests/OID_interpretations.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if value_str == row['OID']:
+                    log_message_parts.append(f'Interpreted OID: {row["text"]}')
+                    break
+
+    log_message = ', '.join(log_message_parts)  # Join parts with ', '
+    print(log_message)
+    logging.info(log_message)
+
+def main(masterpassword):
+    #implement the password logic later, just make everything work now
+    
+    # Initialize SNMP engine
+    snmpEngine = engine.SnmpEngine()
+
+    # Add SNMP users from CSV
+    csv_file_path = '../REMT/Tests/network monitoring/snmp tests/snmp_users.csv'
+    add_snmp_users_from_csv(csv_file_path, snmpEngine)
+
+    # Transport setup
+    # UDP over IPv4
+    config.addTransport(
+        snmpEngine,
+        udp.domainName,
+        udp.UdpTransport().openServerMode(('0.0.0.0', 162))
+    )
+
+    # Register SNMP Application at the SNMP engine
+    ntfrcv.NotificationReceiver(snmpEngine, cbFun)
+
+    # Run I/O dispatcher which would receive queries and send confirmations
+    try:
+        snmpEngine.transportDispatcher.jobStarted(1)  # this job would never finish
+        snmpEngine.transportDispatcher.runDispatcher()
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        snmpEngine.transportDispatcher.closeDispatcher()
+
+if __name__ == "__main__":
+    masterpassword = 'temp'
+    main(masterpassword)
